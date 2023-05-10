22a851ff3ba1f7e49d8d6e2a9ec3f2ab41eede67
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-10 17:48:15 -0300

    Update

--- server.py
+++ server.py
@@ -4,6 +4,9 @@ server = Flask(__name__)
 
 @server.route('/')
 def index():
+    return render_template('under.html')
+@server.route('/preview/status/website/geb')
+def preview():
     return render_template('index.html')
 
 if __name__ == '__main__':
