60babf07da790bee6f22d8bdb9ffda638df025f2
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-10 12:29:33 -0300

    Moved to Flask Application

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect, request
+
+server = Flask(__name__)
+
+@server.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    server.run()
