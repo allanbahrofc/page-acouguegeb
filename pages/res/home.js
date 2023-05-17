f721b19027aa43338e0e5d519b1cf2d4a76bb40f
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-17 20:23:34 -0300

    Updated in Javascript

--- pages/res/home.js
+++ pages/res/home.js
@@ -1,12 +1,16 @@
 const estoqueProdutos = {
     porco: [
         { idProduto: 01, nomeProduto: "Costela de Porco s/pele", categoriaProduto: "Suíno", valorKg: "a" },
-        { idProduto: 02,nomeProduto: "Costela de Porco c/pele", categoriaProduto: "Suíno", valorKg: "a" },
-        { idProduto: 03,nomeProduto: "Pernil de Porco", categoriaProduto: "Suíno", valorKg: "14.99" },
-        { idProduto: 04,nomeProduto: "Paleta de Porco c/pele ", categoriaProduto: "Suíno", valorKg: "14.99" }],
+        { idProduto: 02, nomeProduto: "Costela de Porco c/pele", categoriaProduto: "Suíno", valorKg: "a" },
+        { idProduto: 03, nomeProduto: "Pernil de Porco", categoriaProduto: "Suíno", valorKg: "14.99" },
+        { idProduto: 04, nomeProduto: "Paleta de Porco c/pele ", categoriaProduto: "Suíno", valorKg: "14.99" }],
     bovino: [
         {},
         {},
         {}]
 }
-console.log(estoqueProdutos)
+
+const seletor = document.getElementsByClassName('side-item')
+seletor[0].addEventListener('click', () => {
+    console.log(seletor[0].textContent = 'a')
+})
