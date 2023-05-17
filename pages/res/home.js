22b4cf81c3395371955d2295cb0001f78f4fe71a
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-17 17:38:15 -0300

    Update icons font

--- pages/res/home.js
+++ pages/res/home.js
@@ -1,9 +1,12 @@
 const estoqueProdutos = {
     porco: [
-        { nomeProduto: "Costela de Porco s/pele", categoriaProduto: "Suíno", valorKg: "a" },
-        { nomeProduto: "Costela de Porco c/pele", categoriaProduto: "Suíno", valorKg: "a" },
-        { nomeProduto: "Pernil de Porco", categoriaProduto: "Suíno", valorKg: "24.50" },
-        { nomeProduto: "Paleta de Porco", categoriaProduto: "Suíno", valorKg: "14.99" }],
-    bovino: [{}]
+        { idProduto: 01, nomeProduto: "Costela de Porco s/pele", categoriaProduto: "Suíno", valorKg: "a" },
+        { idProduto: 02,nomeProduto: "Costela de Porco c/pele", categoriaProduto: "Suíno", valorKg: "a" },
+        { idProduto: 03,nomeProduto: "Pernil de Porco", categoriaProduto: "Suíno", valorKg: "14.99" },
+        { idProduto: 04,nomeProduto: "Paleta de Porco c/pele ", categoriaProduto: "Suíno", valorKg: "14.99" }],
+    bovino: [
+        {},
+        {},
+        {}]
 }
 console.log(estoqueProdutos)
