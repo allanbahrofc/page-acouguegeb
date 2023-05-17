ab9f7599b759a99eff9e43fbab966d9d34b51e27
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-17 12:32:29 -0300

    Update to v0.1.0.1

---
+++ pages/res/home.js
@@ -0,0 +1,9 @@
+const estoqueProdutos = {
+    porco: [
+        { nomeProduto: "Costela de Porco s/pele", categoriaProduto: "Suíno", valorKg: "a" },
+        { nomeProduto: "Costela de Porco c/pele", categoriaProduto: "Suíno", valorKg: "a" },
+        { nomeProduto: "Pernil de Porco", categoriaProduto: "Suíno", valorKg: "24.50" },
+        { nomeProduto: "Paleta de Porco", categoriaProduto: "Suíno", valorKg: "14.99" }],
+    bovino: [{}]
+}
+console.log(estoqueProdutos)
