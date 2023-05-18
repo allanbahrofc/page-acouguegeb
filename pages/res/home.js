eb1a036056b69e7bc9f9624d8ac2e6a63ddb539d
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-18 12:23:30 -0300

    Update

--- pages/res/home.js
+++ pages/res/home.js
@@ -2,11 +2,11 @@ const stockBehavior = {}
 
 const sideItemsBehavior = document.getElementsByClassName('side-item')
 const contentBehavior = document.querySelector('.content')
+const gridBehavior = document.querySelector('.grid')
 
 window.addEventListener('load',() => {
-    contentBehavior.remove()
 })
 
 sideItemsBehavior[0].addEventListener('click', () => {
-    console.log(sideItemsBehavior[0].textContent = 'a')
+    
 })
