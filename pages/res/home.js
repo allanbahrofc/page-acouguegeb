a98a5ca67f2e207219d754435c7c01480393f0bf
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-18 10:01:44 -0300

    Update to use json e updating the stylesheet from page

--- pages/res/home.js
+++ pages/res/home.js
@@ -1,16 +1,12 @@
-const estoqueProdutos = {
-    porco: [
-        { idProduto: 01, nomeProduto: "Costela de Porco s/pele", categoriaProduto: "Suíno", valorKg: "a" },
-        { idProduto: 02, nomeProduto: "Costela de Porco c/pele", categoriaProduto: "Suíno", valorKg: "a" },
-        { idProduto: 03, nomeProduto: "Pernil de Porco", categoriaProduto: "Suíno", valorKg: "14.99" },
-        { idProduto: 04, nomeProduto: "Paleta de Porco c/pele ", categoriaProduto: "Suíno", valorKg: "14.99" }],
-    bovino: [
-        {},
-        {},
-        {}]
-}
+const stockBehavior = {}
 
-const seletor = document.getElementsByClassName('side-item')
-seletor[0].addEventListener('click', () => {
-    console.log(seletor[0].textContent = 'a')
+const sideItemsBehavior = document.getElementsByClassName('side-item')
+const contentBehavior = document.querySelector('.content')
+
+window.addEventListener('load',() => {
+    contentBehavior.remove()
+})
+
+sideItemsBehavior[0].addEventListener('click', () => {
+    console.log(sideItemsBehavior[0].textContent = 'a')
 })
