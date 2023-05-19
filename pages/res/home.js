91e9ffd7586a0d65efe5d4fb45a6e3ad420a4813
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-05-19 12:26:27 -0300

    Update

--- pages/res/home.js
+++ pages/res/home.js
@@ -3,6 +3,7 @@ const stockBehavior = {}
 const sideItemsBehavior = document.getElementsByClassName('side-item')
 const contentBehavior = document.querySelector('.content')
 const gridBehavior = document.querySelector('.grid')
+const categoryItems = document.getElementsByClassName('category-item')
 
 window.addEventListener('load',() => {
 })
