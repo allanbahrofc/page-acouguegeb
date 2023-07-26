d77f60b47815c8fca53ebdac7216a49f9cc94bf5
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-07-26 12:21:58 -0300

    Added new Features

---
+++ templates/res/home.js
@@ -0,0 +1,10 @@
+// const grid = document.querySelector('.grid')
+// const list = document.getElementsByClassName('category-item')
+
+// for (let i = 0; i < list.length; i++) {
+//     const element = list[i];
+//     element.addEventListener('click', ()=>{
+//         console.log(element.classList.toggle('selected'))
+//     })
+    
+// }
