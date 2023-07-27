c5ab169dc4515077af9d879efb62606062ac6e12
Allan Bruno de Souza Bahr <allanbrunodesouzabahr@gmail.com>
2023-07-27 18:57:12 -0300

    Added new Features

--- templates/res/home.js
+++ templates/res/home.js
@@ -1,14 +1,34 @@
-const grid = document.querySelector('.grid')
-const list = document.getElementsByClassName('category-item')
+// const grid = document.querySelector('.grid')
+// const list = document.getElementsByClassName('category-item')
+// const gridItems = document.getElementsByClassName('grid-item')
 
-for (let i = 0; i < list.length; i++) {
-    const element = list[i];
-    element.addEventListener('click', ()=>{
-        element.classList.toggle('selected')
-        setTimeout(()=>{
-            element.classList.remove('selected')
-        }, 4000)
-    })
+// for (let i = 0; i < list.length; i++) {
+//     const element = list[i];
+//     element.addEventListener('click', ()=>{
+//         element.classList.toggle('selected')
+//         switch(element.id.valueOf()){
+//             case 'aves':
+//                 console.log('Aves')
+//                 break
+//             case 'ofertas':
+//                 console.log('Ofertas')
+//                 break
+//             case 'mercado':
+//                 console.log('Mercado')
+//                 break
+//             case 'suino':
+//                 console.log('Suíno')
+//                 break
+//             case 'bovino':
+//                 console.log('bovino')
+//                 break
+                
+//             }
+
+//         setTimeout(()=>{
+//             element.classList.remove('selected')
+//         }, 2000)
+//     })
     
     
-}
+// }
