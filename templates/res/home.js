e5f2b1df76fe0142fc7807f5e2580b7ef8272bcb
Allan Bruno de Souza Bahr <madebyallanbahr@gmail.com>
2023-10-29 12:06:30 -0300

    Updating the performance

--- templates/res/home.js
+++ templates/res/home.js
@@ -1,34 +1,34 @@
-const grid = document.querySelector('.grid')
-const list = document.getElementsByClassName('category-item')
-const gridItems = document.getElementsByClassName('grid-item')
+// const grid = document.querySelector('.grid')
+// const list = document.getElementsByClassName('category-item')
+// const gridItems = document.getElementsByClassName('grid-item')
 
-for (let i = 0; i < list.length; i++) {
-    const element = list[i];
-    element.addEventListener('click', ()=>{
-        element.classList.toggle('selected')
-        switch(element.id.valueOf()){
-            case 'aves':
-                console.log('Aves')
-                break
-            case 'ofertas':
-                console.log('Ofertas')
-                break
-            case 'mercado':
-                console.log('Mercado')
-                break
-            case 'suino':
-                console.log('Suíno')
-                break
-            case 'bovino':
-                console.log('bovino')
-                break
+// for (let i = 0; i < list.length; i++) {
+//     const element = list[i];
+//     element.addEventListener('click', ()=>{
+//         element.classList.toggle('selected')
+//         switch(element.id.valueOf()){
+//             case 'aves':
+//                 console.log('Aves')
+//                 break
+//             case 'ofertas':
+//                 console.log('Ofertas')
+//                 break
+//             case 'mercado':
+//                 console.log('Mercado')
+//                 break
+//             case 'suino':
+//                 console.log('Suíno')
+//                 break
+//             case 'bovino':
+//                 console.log('bovino')
+//                 break
                 
-            }
+//             }
 
-        setTimeout(()=>{
-            element.classList.remove('selected')
-        }, 2000)
-    })
+//         setTimeout(()=>{
+//             element.classList.remove('selected')
+//         }, 2000)
+//     })
     
     
-}
+// }
